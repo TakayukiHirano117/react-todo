9eca927d8ac91bf73e9b932a0c7fa5b21a15f913
TakayukiHirnao117 <ikusa.hirano@icloud.com>
2024-12-01 15:36:00 +0900

    ♻️ ✨ todoの削除機能の実装・既存のstate名の調整・todoIdのstate管理

--- src/App.tsx
+++ src/App.tsx
@@ -3,41 +3,44 @@ import './App.css'
 
 function App() {
 
-  // const todos = [
-  //   { id: 1, title: 'todo1' },
-  //   { id: 2, title: 'todo2' },
-  //   { id: 3, title: 'todo3' }
-  // ]
-
   type Todo = {
     id: number;
     title: string;
   };
 
-  // 新規投稿機能
-  // todoを保持するstateを作成　✅
-  // inputから入力された値を取得して、todosに追加する
-  // buttonがクリックされたら、入力された値をtodosに追加する
-  // buttonがクリックされたら、inputの値を空にする
+  // todo削除機能
+  // 削除ボタンが押されたらそのtodoを削除する
+  // filterを使って、削除ボタンが押されたtodo以外のtodoを抽出する
+  // 削除ボタンのonClickイベントに削除処理の関数を追加する
+
+  // todoIdの実装
+  // 現状、削除するとkeyのidがずれてしまい、同じidを持つtodoが存在する可能性がある
+  // todoのidを別のstateで管理して、削除時にそのidを使って削除するtodoを特定する
 
   const [todos, setTodos] = useState<Todo[]>([])
-  const [title, setTitle] = useState<string>('')
+  const [todoTitle, setTodoTitle] = useState<string>('')
+  const [todoId, setTodoId] = useState(todos.length + 1)
 
   const handleTitleChange = (e: ChangeEvent<HTMLInputElement>) => {
-    setTitle(e.target.value)
-    // console.log(title)
+    setTodoTitle(e.target.value)
   }
 
   const handleTodoAdd = () => {
-    setTodos([...todos, { id: todos.length + 1, title: title }])
-    setTitle('')
+    setTodos([...todos, { id: todoId, title: todoTitle }])
+    setTodoId(todoId + 1)
+    setTodoTitle('')
+  }
+
+  const handleTodoDelete = (targetTodo: Todo) => {
+    const newTodos = todos.filter((todo) => todo.id !== targetTodo.id)
+    setTodos(newTodos)
   }
 
   return (
     <div>
       <h1>Todoリスト</h1>
       <div>
-        <input type="text" placeholder="タイトル" value={title} onChange={(e) => handleTitleChange(e)} />
+        <input type="text" placeholder="タイトル" value={todoTitle} onChange={(e) => handleTitleChange(e)} />
         <button onClick={handleTodoAdd}>追加</button>
       </div>
       <ul>
@@ -45,7 +48,7 @@ function App() {
           todos.map((todo) => (
             <li key={todo.id}>{todo.title}
               <button>編集</button>
-              <button>削除</button>
+              <button onClick={() => { handleTodoDelete(todo) }}>削除</button>
             </li>
           ))
         }
